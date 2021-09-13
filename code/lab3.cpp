dc7a071cb125aa63c2688a4270de924fe2d9f8b7
qqqq4u <danila16102003@rambler.ru>
2021-09-13 22:04:15 +0300

    done all tasks

--- code/lab3.cpp
+++ code/lab3.cpp
@@ -15,6 +15,7 @@ int main() {
 
             int n, m, sum = 0;
             while (resume) {
+                sum = 0;
 
                 //n - count of numbers
                 cout << "n: m:  \n";
@@ -38,18 +39,20 @@ int main() {
 
         case 2: //task 2
         {
-            int a, product = 1;
+            double a, product = 1.0;
             cout << "a: ";
             cin >> a;
             if (a >= 0) {
                 for (int i = 2; i <= 8; i += 2) {
                     product *= i * i;
                 }
+                product -= a;
             } else {
                 for (int i = 3; i <= 9; i += 3) {
                     product *= i - 2;
                 }
             }
+            cout << fixed << setprecision(3) << product;
         }
 
             break;
@@ -63,7 +66,7 @@ int main() {
 
         case 3: {
             {
-                cout << setw(3) << 'x' << setw(9) << 'y'  << setw(8) << 's' << setw(4) << 'n'<< endl;
+                cout << setw(3) << 'x' << setw(9) << 'y' << setw(8) << 's' << setw(4) << 'n' << endl;
                 double y, s, s1, eps = 0.000001;
                 int n;
                 for (double x = 0; x <= 1; x += 0.2) {
@@ -79,6 +82,30 @@ int main() {
                     }
                     cout << setw(3) << x << '|' << setw(9) << y << '|' << setw(8) << s << '|' << setw(1) << n << endl;
                 }
+            }
+        }
+            break;
+        case 4: {
+            bool resume = true;
+            while (resume) {
+                int n = 0;
+                double x;
+                cout << "n: ";
+                cin >> n;
+                cout << '\n' << "x:";
+                cin >> x;
+                double y = 1.0 + x * x / 2, y1 = x * x / 2;
+                for (int i = 2; i <= n; i++) {
+                    y1 *= x * x / ((2 * i) * (2 * i - 1));
+                    y += y1;
+                    if (i == 3 || i == 5 || i == 10) {
+                        cout << "Частичная сумма при " << i << " слагаемых = " << y << endl;
+                    }
+
+                }
+                cout << "y: " << y << endl;
+                cout << "do you want to continue?(1/0)";
+                cin >> resume;
             }
         }
             break;
