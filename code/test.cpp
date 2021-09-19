3e42f88d91c3d54c1ce2d9794221f62c98f6fbf3
qqqq4u <danila16102003@rambler.ru>
2021-09-19 22:58:11 +0300

    done report v.1 without scheme

--- code/test.cpp
+++ code/test.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-int mainq() {
+int main()
+{
+    cout << setw(3) << 'x' << setw(9) << 'y' << setw(8) << 's' << setw(4) << 'n' << endl;
     double y, s, s1, eps = 0.000001;
     int n;
-    for (double x = 0; x <= 1; x += 0.2) {
-        y = cos(2 * x - 1);
-        s = 1.0 - pow(2 * x, 2) / 2;
-        n = 1;
-        s1 = pow(2 * x, 2) / 2;
-        while (abs(s1) > eps) {
-            n++;
-            s1 = s1 * (-(4 * x * x / (2 * n * (2 * n - 1))));
+    for (double x = 0; x <= 1; x += 0.2)
+    {
+        y = cosh(x);
+        double s = 1;
+        double s1 = 1;
+        int n = 1;
+        while (abs(s1)>=eps)
+        {
+             s1 *= x * x;
+            s1 /= 2 * n * (2 * n - 1);
             s += s1;
-            cout << y << ' ' << s << ' ' << n << endl;
+            n++;
         }
-
-
+     
+        cout << setw(3) << x << '|' << setw(9) << y << '|' << setw(8) << s << '|' << setw(1) << n << endl;
     }
-
 
     return 0;
 }
